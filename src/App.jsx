876b5fc52021f416acc4ae07ff100cda67c96c59
barry <mbrsyr@yahoo.com>
2022-11-11 10:36:08 +0100

    dynamical data table

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import { ChatEngine } from 'react-chat-engine';
+import { useState } from 'react';
 import './App.css';
+import data from './mock-data.json';
 
 const App = () => {
-  return <ChatEngine height='100vh' projectID='' userName='' userSecret='' />;
+  const [contacts, setContacts] = useState(data);
+
+  return (
+    <div className='app-container'>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            const { fullName, email, address, phoneNumber } = contact;
+            return (
+              <tr>
+                <td>{fullName}</td>
+                <td>{address}</td>
+                <td>{phoneNumber}</td>
+                <td>{email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;
